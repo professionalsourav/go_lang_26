0eb3ed39b4660b6dd52b1483de0dd56a637399b8
professionalsourav <chakravarttysourav@gmail.com>
2025-12-22 14:29:14 +0530

    scope is done

--- 9scope/main.go
+++ 9scope/main.go
@@ -3,7 +3,31 @@ package main
 
 import "fmt"
 
+var g int //global variable 
+
+var x int = 25 //variable shadowing
 
 func main (){
-	fmt.Println("hello")
+	//local variable
+
+	var a , b, c int
+
+	a = 10
+	b = 20
+	c = a+b
+	fmt.Printf("local variable \n")
+
+	fmt.Printf("a = %d, b = %d, c = %d\n", a, b, c)
+
+
+	//global variable
+  fmt.Printf("global variable \n")
+	g = b-a 
+
+	fmt.Printf("a = %d, b = %d, global variable g = %d\n", a, b , g)
+
+	 x := 100
+
+	 fmt.Printf("x = %d\n", x)
+
 }
