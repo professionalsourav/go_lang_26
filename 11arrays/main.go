61f59ff0910efff1ec86e8bc5f7a9b0805c3cd1e
professionalsourav <chakravarttysourav@gmail.com>
2025-12-30 16:34:25 +0530

    array is not completed

---
+++ 11arrays/main.go
@@ -0,0 +1,17 @@
+package main
+
+// import (
+// 	"fmt"
+// )
+
+
+//declaring array
+var balance [10]float32
+
+// initializing array at declaration
+var balance =[5]float32[12.2, 23.34 , 45.34, 65.23, 10.20 ]
+
+
+//initialize while omitting the size of the array elements
+
+var balance =[]float32[12.2, 23.34 , 45.34, 65.23, 10.20 ]
